9eb63e951bcee1afef363a0f6a3e428afc031678
Mukatay Ilyas <mr.mukatai@gmail.com>
2022-07-08 18:45:42 +0600

    first commit

--- satus/settings.py
+++ satus/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = str(os.environ.get('DEBUG')) == '1'
 
 ALLOWED_HOSTS = (
-    os.environ.get('ALLOWED_HOST'),
+    os.environ.get('ALLOWED_HOST')
 )
 
 # Application definition
